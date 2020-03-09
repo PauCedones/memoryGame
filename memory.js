a8f9f1661f6bbf61baa5ac2890b6d80e68de6f36
Paula <cedones@gmail.com>
2020-03-09 19:23:29 -0300

    agregando funcionalidad a los botones de jugar y reiniciar

--- memory.js
+++ memory.js
@@ -1,5 +1,5 @@
 // testeando si esta bien vinculado
-console.log("memory.js file reached");
+console.log("memory.js esta vinculado");
 
 // array con titulos
 const titles_array = ["title1,title2,title3,title4,title5,title6,title7,title8,title9,title10"];
@@ -28,3 +28,87 @@ var duplicate_click = false;
 
 // empezar el juego, true o false
 var game_start = false;
+
+// haciendo los event listeners a los botones
+// boton COMENZAR JUEGO
+const start_game = () => {
+    console.log("Comenzo el juego");
+    //esta funcion se usar para jugar y reiniciar el juego
+    reset_var_shallow()
+}
+//BOTON REINICIAR JUEGO DE FORMA RANDOM
+const reset_game = () => {
+    console.log("Se reinició el juego");
+    //esta funcion se usar para jugar y reiniciar el juego
+    reset_var_shallow()
+}
+
+document.querySelector("#start").addEventListener("click", start_game);
+
+document.querySelector("#reset").addEventListener("click", reset_game);
+
+// Reiniciar variables si hiciste click en jugar o reiniciaste el juego
+const reset_var_shallow = () =>{
+    //resetear el ultimo color clickeado
+    last_color = "",
+        click= 0,
+        clicked_titles = [],
+        duplicate_click = false;
+        //start game si la variable es verdadera
+    game_start = true;
+    for(var i=1; i<11; i++){
+        var element = "title"+ i;
+        document.getElementById(element).style.backgroundColor = "greenyellow";
+    }
+}
+
+
+// Reiniciar variables que deberian ser reseteadas si matcheaste mal o reiniciaste 
+
+
+
+
+
+
+
+
+
+
+
+
+// ID title eventListener (click)
+const titles = (title) => {
+    console.log(title);
+}
+
+document.querySelector("#title1").addEventListener("click",function(){
+    titles("title1");
+});
+document.querySelector("#title2").addEventListener("click",function(){
+    titles("title2");
+});
+document.querySelector("#title3").addEventListener("click",function(){
+    titles("title3");
+});
+document.querySelector("#title4").addEventListener("click",function(){
+    titles("title4");
+});
+document.querySelector("#title5").addEventListener("click",function(){
+    titles("title5");
+});
+document.querySelector("#title6").addEventListener("click",function(){
+    titles("title6");
+});
+document.querySelector("#title7").addEventListener("click",function(){
+    titles("title7");
+});
+document.querySelector("#title8").addEventListener("click",function(){
+    titles("title8");
+});
+document.querySelector("#title9").addEventListener("click",function(){
+    titles("title9");
+});
+document.querySelector("#title10").addEventListener("click",function(){
+    titles("title10");
+});
+
