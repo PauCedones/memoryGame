181013812d6a31e11a5198cc24295bc704c650ac
Paula <cedones@gmail.com>
2020-03-09 21:29:17 -0300

    haciendo random el juego

--- memory.js
+++ memory.js
@@ -32,7 +32,7 @@ var game_start = false;
 // haciendo los event listeners a los botones
 // boton COMENZAR JUEGO
 const start_game = () => {
-    console.log("Comenzo el juego");
+    console.log("El juego está activo");
     //esta funcion se usar para jugar y reiniciar el juego
     reset_var_shallow()
 }
@@ -64,22 +64,78 @@ const reset_var_shallow = () =>{
 
 
 // Reiniciar variables que deberian ser reseteadas si matcheaste mal o reiniciaste 
+const reset_var_deep = () =>{
+    //
+    var red =0;
+    var blue =0;
+    var yellow =0;
+    var purple =0;
+    var black =0;
 
+    // arrays vacios para meter nuevos tiles
+    red_array = [],
+    blue_array = [],
+    yellow_array = [],
+    purple_array = [],
+    black_array = [];
 
-
-
-
-
-
-
-
-
-
-
-// ID title eventListener (click)
-const titles = (title) => {
-    console.log(title);
+    for (var i=0; i<10; i++){
+        var number= Math.floor(Math.random()*5);
+        
+        if(number == 0){
+           if(red < 2){
+                document.getElementById(titles_array[i]).style.backgroundColor = "Red";            
+                red++;
+                red_array.push(titles_array[i]);
+           }
+           else{
+               i--;
+           } 
+        }
+    }
 }
+
+// cargar la funcion, poniendo el juego listo cuando la web haya cargado
+window.onload = function(){
+    reset_var_deep();
+    console.log("la carga esta completa");
+}
+
+
+
+
+
+
+
+
+
+
+// Principal funcion de todo el juego
+const titles = (title) => {
+    // si el juego esta activo
+    if(game_start){
+        console.log("El juego empezó");
+
+        //chequea si el title tocando fue tocado anteriormente
+        for (var i=0; i< clicked_titles.length; i++){
+            if(title == clicked_titles[i]){
+                duplicate_title = true;
+                console.log("cuadrado duplicado")
+            }
+        }
+        //si el cuadrado no ha sido clickeado anteriormente durante el juego
+        if(!duplicate_click){
+            clicked_titles.push(title);
+        }
+        //Reiniciando el click duplicado
+        duplicate_click = false;
+    }
+    // si el juego no esta activo
+    else{
+        console.log("El juego NO empezo")
+    }
+}
+// ID title eventListener (click)
 
 document.querySelector("#title1").addEventListener("click",function(){
     titles("title1");
